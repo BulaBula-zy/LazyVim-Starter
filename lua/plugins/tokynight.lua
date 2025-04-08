dd747996d4044439d49aa0a59dc63ebd369e5324
Zyyy <e1351057@u.nus.edu>
2025-04-08 18:52:03 +0800

    [dev] first commit

---
+++ lua/plugins/tokynight.lua
@@ -0,0 +1,15 @@
+if true then
+  return {}
+end
+
+-- BUG: not sooth
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
