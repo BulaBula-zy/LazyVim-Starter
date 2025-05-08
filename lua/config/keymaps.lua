69dc8bafa0bc7f88b26e0d12cde7913323db262f
Zyyy <e1351057@u.nus.edu>
2025-05-08 17:45:49 +0800

    [dev] mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,4 +5,14 @@
 local map = vim.keymap.set
 
 map("i", "jk", "<Esc>", { desc = "Escape insert mode", silent = true })
+
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save File", silent = true })
+
+map("n", "bh", "^", { desc = "Move to the beginning of the line" })
+map("n", "el", "$", { desc = "Move to the end of the line" })
+
+-- map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half a screen and center the cursor" })
+-- map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half a screen and center the cursor" })
+
+map("n", "eb", "viw", { desc = "Select inner word" })
+map("n", "EB", "viW", { desc = "Select inner WORD" })
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,7 +6,7 @@ return {
         for _, mapping in ipairs(group) do
           if mapping[1] == "<leader>w" then
             mapping[1] = "<leader>W"
-            mapping.group = "windows" -- 可选，保持组名
+            mapping.group = "windows"
           end
         end
       end
