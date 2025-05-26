68223b8318da1a13be90eb709d9281839c6ba7f5
Zyyy <e1351057@u.nus.edu>
2025-05-26 09:40:15 +0800

    [dev] add leetcode.lua plugin

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,30 @@
+return {
+  "kawre/leetcode.nvim",
+  -- build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "ibhagwan/fzf-lua",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    ---@type lc.lang
+    lang = "python3",
+    cn = { -- leetcode.cn
+      enabled = true, ---@type boolean
+      translator = false, ---@type boolean
+      translate_problems = false, ---@type boolean
+    },
+    ---@type boolean
+    image_support = false,
+    injector = { ---@type table<lc.lang, lc.inject>
+      ["python3"] = {
+        before = true,
+      },
+      ["cpp"] = {
+        before = { "#include <bits/stdc++.h>", "using namespace std;" },
+        after = "int main() {}",
+      },
+    },
+  },
+}
